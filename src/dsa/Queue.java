27306b8b829819feabc4863e48e81496da53db58
Uyanwune Chidozie Chukwuebuka <talk2don90@gmail.com>
2022-03-03 23:41:47 +0100

    queue add method complete

--- src/dsa/Queue.java
+++ src/dsa/Queue.java
@@ -10,4 +10,21 @@ public class Queue {
         firstElement = -1;
         lastElement = -1;
     }
+
+    public void addToQueue(int element){
+        if (lastElement == queue.length - 1){
+            System.out.println("Queue is full");
+        }
+
+        if (firstElement == -1){
+            firstElement = 0; lastElement = 0;
+            queue[0] = element;
+        }
+        lastElement++;
+        queue[lastElement] = element;
+    }
+
+    public int removeFromQueue(){
+        if ()
+    }
 }
